3300c8213fe1298a1fbc67a57d1237f9cca83256
Esodey <esodey@gmail.com>
2019-04-30 15:43:37 -0700

    Implement code for datastore readAll and readOne

--- datastore/index.js
+++ datastore/index.js
@@ -12,7 +12,7 @@ exports.create = (text, callback) => {
     if (err) {
       throw ('error getting unique id');
     } else {
-      let filePath = path.join(exports.dataDir, `${id}.txt`); // 
+      let filePath = path.join(exports.dataDir, `${id}.txt`); 
       fs.writeFile(filePath, text, (err) => {
         if (err) {
           throw ('error writing file');
@@ -29,20 +29,48 @@ invoke getNextUniqueId to generate a file path for the
 */
 
 exports.readAll = (callback) => {
-  var data = _.map(items, (text, id) => {
-    return { id, text };
+  let path = 
+  fs.readdir(exports.dataDir, (err, todoList) => {
+    if (err) {
+      throw ('error reading directory');
+    } else {
+      let list = _.map(todoList, (element) => {
+        let id = element.slice(0, -4);
+        let text = element.slice(0, -4); // refactor this!
+        return { id, text };
+      });
+      // console.log(list);
+      callback(null, list);
+    }
   });
-  callback(null, data);
+
+  // var data = _.map(items, (text, id) => {
+  //   return { id, text }; // { id: id, text: tex }
+  // });
+  // callback(null, data);
+
+  // retrieve files with fs.readDir => array of files /00001.txt
+  // on err -> throw error
+  // on success, pass array of files into callback
+
 };
 // should retrieve all the contents of all stored todo list files
 
 exports.readOne = (id, callback) => {
-  var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback(null, { id, text });
-  }
+  let filePath = path.join(exports.dataDir, `${id}.txt`); 
+  fs.readFile(filePath, (err, text) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, {id, text: text.toString('utf8')});
+    }
+  });
+  // var text = items[id];
+  // if (!text) {
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback(null, { id, text });
+  // }
 };
 // should retrieve the contents of one todo list file
 
